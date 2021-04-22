fffa8c0815f752a32726615856227a56e5af0894
Osiris <potterosiris@gmail.com>
2021-04-22 19:37:06 -0400

    error modal loading spinner modal

---
+++ template-client/src/shared/UIElements/ErrorModal.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Button from "../FormElements/Button"
+
+import Modal from "./Modal"
+
+const ErrorModal = props => {
+
+    return (
+        <Modal
+            onCancel={props.onClear}
+            header="An Error Occurred!"
+            show={!!props.error}
+            footer={<Button onClick={props.onClear}> okay </Button>}
+        >
+            <p>{props.error}</p>
+        </Modal>
+    )
+}
+
+export default ErrorModal
